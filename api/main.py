fae7eb09f565200b9bf1b45deb3e5f6286627b55
PabloGMD <pablogmdsoftware@gmail.com>
2026-05-20 13:54:54 +0200

    feature: add read_users endpoint

--- api/main.py
+++ api/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from sqlmodel import select
+from typing import Annotated
+from dependencies import SessionDep
+from models import User, UserPublic
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/users/", response_model=list[UserPublic])
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
