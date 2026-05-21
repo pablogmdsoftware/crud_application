e004b4d52356e457217f8ce96600405613bc5acd
PabloGMD <pablogmdsoftware@gmail.com>
2026-05-21 14:28:47 +0200

    fix: remove id column from csv files

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -1,4 +1,4 @@
-\COPY users(id, name, email, password) FROM '/data/user_data.csv' DELIMITER ',' CSV HEADER;
-\COPY projects(id, name, owner_id) FROM '/data/project_data.csv' DELIMITER ',' CSV HEADER;
-\COPY tasks(id, title, description, status, project_id, assigned_to) FROM '/data/task_data.csv' DELIMITER ',' CSV HEADER;
-\COPY comments(id, content, task_id, user_id) FROM '/data/comment_data.csv' DELIMITER ',' CSV HEADER;
+\COPY users(name, email, password) FROM '/data/user_data.csv' DELIMITER ',' CSV HEADER;
+\COPY projects(name, owner_id) FROM '/data/project_data.csv' DELIMITER ',' CSV HEADER;
+\COPY tasks(title, description, status, project_id, assigned_to) FROM '/data/task_data.csv' DELIMITER ',' CSV HEADER;
+\COPY comments(content, task_id, user_id) FROM '/data/comment_data.csv' DELIMITER ',' CSV HEADER;
