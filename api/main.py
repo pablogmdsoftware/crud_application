1203fd1ae5ca1dd221f4f0be28c4ef2c4e6b52b3
PabloGMD <pablogmdsoftware@gmail.com>
2026-05-25 09:52:21 +0200

    feature: add update_user endpoint

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Query, HTTPException
 from sqlmodel import select
 from typing import Annotated
 from dependencies import SessionDep
-from models import User, UserPublic, UserCreate
+from models import User, UserPublic, UserCreate, UserUpdate
 
 app = FastAPI()
 
@@ -45,3 +45,15 @@ def delete_user(user_id: int, session: SessionDep):
     session.delete(user)
     session.commit()
     return {"ok": True}
+
+@app.patch("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserUpdate, session: SessionDep):
+    user_db = session.get(User, user_id)
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_data = user.model_dump(exclude_unset=True)
+    user_db.sqlmodel_update(user_data)
+    session.add(user_db)
+    session.commit()
+    session.refresh(user_db)
+    return user_db
