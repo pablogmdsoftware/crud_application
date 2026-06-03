d92a9f2b3f5d5c766442786fd702a6cae8e54423
PabloGMD <pablogmdsoftware@gmail.com>
2026-06-03 12:40:54 +0200

    feature: add api service to docker-compose file

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine
 
-engine = create_engine(f"postgresql://myuser:mypassword@localhost:5433/mydatabase")
+engine = create_engine(f"postgresql://myuser:mypassword@db:5432/mydatabase")
 
 SECRET_KEY = "df9564678fa80612898b78d7abd7f35477f93dbd23c39932f664285acb4a9834"
 ALGORITHM = "HS256"
