c2c3621ecdeb4f07fbf5316968dc4128f8b05c9a
PabloGMD <pablogmdsoftware@gmail.com>
2026-06-04 11:03:53 +0200

    security: remove hardcoded database engine settings

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,18 @@
 from sqlmodel import create_engine
 
-engine = create_engine(f"postgresql://myuser:mypassword@db:5432/mydatabase")
+def get_secret(secret: str):
+    path = f"/run/secrets/{secret}"
+    try:
+        with open(path, "r") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        raise RuntimeError(f"Secret db_name not found at {path}")
+
+user = get_secret("db_user")
+password = get_secret("db_password")
+name = get_secret("db_name")
+
+engine = create_engine(f"postgresql://{user}:{password}@db:5432/{name}")
 
 SECRET_KEY = "df9564678fa80612898b78d7abd7f35477f93dbd23c39932f664285acb4a9834"
 ALGORITHM = "HS256"
